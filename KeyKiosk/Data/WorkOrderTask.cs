2b8c7e23f649d7647879d43bcc2c05df97688e6a
Rachel <prasadra@sheridancollege.ca>
2025-10-02 16:44:57 -0400

    added workorder crud ops.

--- KeyKiosk/Data/WorkOrderTask.cs
+++ KeyKiosk/Data/WorkOrderTask.cs
@@ -8,7 +8,10 @@
 		public DateTimeOffset EndDate { get; set; }
 		public WorkOrderTaskStatusType Status { get; set; }
 		public int CostCents { get; set; }
-	}
+
+        public int WorkOrderId { get; set; }
+        public WorkOrder WorkOrder { get; set; } = null!;
+    }
 	public enum WorkOrderTaskStatusType
 	{
 		WorkStarted,
